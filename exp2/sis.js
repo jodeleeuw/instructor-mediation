7b2a19bc11c5c1ffcf7c884cd09dd630df7ff3a1
Josh de Leeuw <josh.deleeuw@gmail.com>
2016-08-30 14:49:50 -0400

    bug fixes

--- exp2/sis.js
+++ exp2/sis.js
@@ -7,18 +7,20 @@ var sis_string =
 "<p>The purpose of this study is to better understand how the teaching style of the instructor affects learning and memory.</p>"+
 "<h3>PROCEDURES FOR THE STUDY</h3>"+
 "<p>If you agree to be in the study, you will do the following things: You will study a list of words or symbols in a new language and will be asked to learn and memorize them. You will later be asked to remember what you learned or use that information in a new context.</p>"+
-"<p>There will be two sessions for this experiment. Each session will take up to 35 minutes to complete. </p>"+
+"<p>There will be two sessions for this experiment. The first session will take up to 45 minutes to complete. The second session will take up to 35 minutes to complete. You will complete the second session at the time you chose when you signed up for the study.</p>"+
 "<h3>RISKS AND BENEFITS</h3>"+
 "<p>There are no risks associated with participating in this research.</p>"+
 "<p>You benefit from this experience because you learn something about how an experiment is designed and conducted, what issues are of interest to cognitive scientists, and what kinds of group behaviors emerge when individuals try to reach their goals in an environment that consists largely of other individuals.</p>"+
+"<h3>ALTERNATIVES TO TAKING PART IN THIS STUDY</h3>"+
+"<p>You may choose not to participate in this study and participate in another PBS study or you may write a one page paper to earn the same amount of credit. Please see your instructor for the topic.</p>"+
 "<h3>CONFIDENTIALITY</h3>"+
 "<p>Efforts will be made to keep your personal information confidential.  We cannot guarantee absolute confidentiality.  Your personal information may be disclosed if required by law.  Your identity will be held in confidence in reports in which the study may be published and databases in which results may be stored.</p>"+
 "<p>Organizations that may inspect and/or copy your research records for quality assurance and data analysis include groups such as the study investigator and his/her research associates, the Indiana University Institutional Review Board or its designees, and (as allowed by law) state or federal agencies, specifically the Office for Human Research Protections (OHRP), who may need to access your  research records.</p>"+
 "<h3>PAYMENT</h3>"+
-"<p>You will receive payment for taking part in this study. You will receive one (1) credit for each session you participate in, for a total of 2 (two) credits, for Psychology 101, 102, or 155. Withdrawal prior to the completion of the study will result in no penalty or loss of benefits. You will receive compensation for each session separately at the time of participation. In addition, you will receive a monetary bonus of up to $9 based on your performance in the task at the end of the second session.</p>"+
+"<p>You will receive payment for taking part in this study. You will receive one (1) credit for each session you participate in, for a total of 2 (two) credits, for Psychology 101, 102, or 155. Withdrawal prior to the completion of the study will result in no penalty or loss of benefits. You will receive compensation for each session separately at the time of participation.</p>"+
 "<h3>CONTACTS FOR QUESTIONS OR PROBLEMS</h3>"+
 "<p>For questions about the study, contact the researcher Dr. Robert Goldstone at 812-855-4853 or rgoldsto@indiana.edu.</p>"+
 "<p>For questions about your rights as a research participant or to discuss problems, complaints or concerns about a research study, or to obtain information, or offer input, contact the IU Human Subjects Office at (812) 856-4242 or (800) 696-2949.</p>"+
 "<h3>VOLUNTARY NATURE OF STUDY</h3>"+
 "<p>Taking part in this study is voluntary.  You may choose not to take part or may leave the study at any time.  Leaving the study will not result in any penalty or loss of benefits to which you are entitled.  Your decision whether or not to participate in this study will not affect your current or future relations with Indiana University.</p>"+
-"<p>Form date: February 24, 2016</p>"+
+"<p>Form date: August 25, 2016</p>";
